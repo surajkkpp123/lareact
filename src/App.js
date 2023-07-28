e13c154b4a4d79cc1414b09427b4d36590ac75ad
Suraj Kumar Patel <suraj@ace.online>
2023-07-28 18:15:28 +0530

    some modification done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Component } from 'react';
+import Loading from './Loading';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is Suraj's Website</p>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    //state
+    this.state = {
+      users: [],
+      loading: false
+    }
+    //binding
+    this.handleSubmit = this.handleSubmit(this);
+  }
+
+  getUsers(){
+    this.setState({
+      loading: true
+    });
+    axios('https://randomuser.me/api/?nat=gb&results=5').then(response=>this.setState({
+        users: [... this.state.users , ... response.data.results],
+        loading: false
+    }));
+  }
+
+  componentWillMount(){
+    this.getUsers()
+  }
+
+  handleSubmit(){
+    this.getUsers();
+  }
+
+  render() {
+    return (<div className="App">  
+            <form onSubmit = {this.handleSubmit}>
+                <input type = "submit" value= "Load users"></input>
+            </form>
+          {!this.state.loading ? this.state.users.map(user=>
+            <div key={user.email}>
+              <h3>{user.name.first}</h3>
+              <h4>{user.email}</h4>
+              <hr></hr>
+            </div>) : (<Loading/>)}
+          </div>);
+  }
 }
 
 export default App;
